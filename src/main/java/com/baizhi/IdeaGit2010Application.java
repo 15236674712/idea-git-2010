aa6b8df49e553de34e19b4782103cd91a2e6f8e4
zhangcn <15236674712@163.com>
2021-06-06 17:38:59 +0800

    项目基础环境搭建完成

---
+++ src/main/java/com/baizhi/IdeaGit2010Application.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaGit2010Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaGit2010Application.class, args);
+    }
+
+}
