d0ac52d0e9043b88d804995adfcec1e387c93893
zhangcn <15236674712@163.com>
2021-06-07 10:42:59 +0800

    小黄:测试类添加代码

--- src/test/java/com/baizhi/IdeaTests.java
+++ src/test/java/com/baizhi/IdeaTests.java
@@ -17,6 +17,13 @@ class IdeaTests {
         String str="aaa";
         int aaa=13;
         System.out.println(aaa);
+
+        String strs="hello";
+
+        int a3=23;
+
+        String s=strs+a3;
+        System.out.println(s);
     }
 
 }
